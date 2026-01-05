ec430b3ca3e85ac952a40ebf93bedf9166f07254
Gustav0547 <jasoncat289@gmail.com>
2026-01-04 20:47:05 -0500

    Update main.py

--- firmware/main.py
+++ firmware/main.py
@@ -16,25 +16,25 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 keyboard.keymap = [
     # Layer 0
-    [KC.LCTRL(KC.P1), KC.LCTRL(KC.P2), KC.LCTRL(KC.P3), KC.NO,
-     KC.LCTRL(KC.P4), KC.LCTRL(KC.P5), KC.LCTRL(KC.P6), KC.TT(1),
-     KC.LCTRL(KC.DEL), KC.LCTRL(KC.END), KC.LCTRL(KC.PGDN), KC.TT(2),
-     KC.LCTRL(KC.INS), KC.LCTRL(KC.HOME), KC.LCTRL(KC.PGUP), KC.TT(3)],
+    [KC.TO(1), KC.TO(2), KC.TO(3), KC.LCTRL(KC.INS),
+     KC.LCTRL(KC.P1), KC.LCTRL(KC.P2), KC.LCTRL(KC.P3), KC.LCTRL(KC.HOME),
+     KC.LCTRL(KC.P4), KC.LCTRL(KC.P5), KC.LCTRL(KC.P6), KC.LCTRL(KC.PGUP),
+     KC.LCTRL(KC.DEL), KC.LCTRL(KC.END), KC.LCTRL(KC.PGDN), KC.TO(0)],
     # Layer 1
-    [KC.LALT(KC.P1), KC.LALT(KC.P2), KC.LALT(KC.P3), KC.TT(0),
-     KC.LALT(KC.P4), KC.LALT(KC.P5), KC.LALT(KC.P6), KC.NO,
-     KC.LALT(KC.P7), KC.LALT(KC.P8), KC.LALT(KC.P9), KC.TT(2),
-     KC.B, KC.NO, KC.NO, KC.TT(3)],
+    [KC.TO(1), KC.TO(2), KC.TO(3), KC. LCTRL(KC.P7),
+     KC.LALT(KC.P1), KC.LALT(KC.P2), KC.LALT(KC.P3), KC.LCTRL(KC.P8),
+     KC.LALT(KC.P4), KC.LALT(KC.P5), KC.LALT(KC.P6), KC.LCTRL(KC.P9),
+     KC.LALT(KC.P7), KC.LALT(KC.P8), KC.LALT(KC.P9), KC.TO(0)],
      # Layer 2
-    [KC.NO, KC.NO, KC.NO, KC.TT(0),
-    KC.NO, KC.NO, KC.NO, KC.TT(1),
-    KC.NO, KC.NO, KC.NO, KC.NO,
-    KC.C, KC.NO, KC.NO, KC.TT(3)],
+    [KC.TO(1), KC.TO(2), KC.TO(3), KC.L,
+     KC.T, KC.Y, KC.U, KC.N,
+     KC.I, KC.O, KC.P, KC.M,
+     KC.H, KC.J, KC.K, KC.TO(0)],
     # Layer 3
-    [KC.NO, KC.NO, KC.NO, KC.TT(0),
-    KC.NO, KC.NO, KC.NO, KC.TT(1),
-    KC.NO, KC.NO, KC.NO, KC.TT(2),
-    KC.D, KC.NO, KC.NO, KC.NO]
+    [KC.TO(1), KC.TO(2), KC.TO(3), KC.F22,
+    KC.F13, KC.F14, KC.F15, KC.F23,
+    KC.F16, KC.F17, KC.F18, KC.F24,
+    KC.F19, KC.F20, KC.F21, KC.TO(0)]
 ]
 
 if __name__ == '__main__':
